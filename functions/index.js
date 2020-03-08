aadb8790b9a44babba502765d28e5ad6cef5080b
Ibrahim Ahmed <ibrahim@ibrahimahmed.ca>
2020-03-07 23:05:26 -0500

    added vote results

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@ const functions = require('firebase-functions');
 
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
+
 admin.initializeApp();
 
 var db = admin.database();
@@ -11,25 +12,23 @@ var ref = db.ref('votes');
 exports.submitVote = functions.https.onCall((data, context) => {
   var votes = data.votes.filter(value => ['one', 'two', 'three', 'four'].includes(value));
   if (!context.auth) {
-    throw new functions.https.HttpsError('failed-precondition', 'You must be signed in ' +
+    throw new functions.https.HttpsError('unauthenticated', 'You must be signed in ' +
         'to vote.');
   }
   const email = context.auth.token.email.replace(/\./g, ',');
   return ref.once('value').then(function(snapshot) {
     if (!email.endsWith("@wrdsb,ca")) {
-      throw new functions.https.HttpsError('failed-precondition', 'You must use a WRDSB ' +
+      throw new functions.https.HttpsError('permission-denied', 'You must use a WRDSB ' +
           'email to vote.');
     }
-	if (snapshot.hasChild(email))
-	{
+	if (snapshot.hasChild(email)) {
 		var vote = snapshot.child(email);
 		if (vote.child('voted').val()) {
 		  throw new functions.https.HttpsError('failed-precondition', 'You have already voted.');
 		}
 	}
     
-    if (votes.length != 2)
-    {
+    if (votes.length != 2) {
       throw new functions.https.HttpsError('failed-precondition', 'You must vote for exactly ' +
           'two candidates.');
     }
@@ -46,4 +45,52 @@ exports.submitVote = functions.https.onCall((data, context) => {
 		context.rawRequest.ip);
     return { text: 'Your vote has been successfully cast.' };
   });
+});
+
+exports.viewResults = functions.https.onCall((data, context) => {
+	if (!context.auth) {
+    throw new functions.https.HttpsError('unauthenticated', 'You must be signed in ' +
+        'to view results.');
+  }
+	const email = context.auth.token.email.replace(/\./g, ',');
+	return ref.once('value').then(function(snapshot) {
+		if (snapshot.hasChild(email + '/admin') && snapshot.child(email + '/admin').val()) {
+			var counter = {};
+			// Count with IP filtering.
+			snapshot.forEach(function(childSnapshot) {
+				if (childSnapshot.child('ip').val() == '199.212.251.10') {
+					var key1 = childSnapshot.child('vote1').val();
+					counter[key1] = (counter[key1] || 0) + 1;
+					var key2 = childSnapshot.child('vote2').val();
+					counter[key2] = (counter[key2] || 0) + 1;
+				}
+			});
+			let cecelia = counter['one'] || 0;
+			let matthew = counter['two'] || 0;
+			let maria = counter['three'] || 0;
+			let sven = counter['four'] || 0;
+			
+			filtered_results = `Cecelia: ${cecelia} <br /> Matthew: ${matthew} <br /> Maria: ${maria} <br /> Sven: ${sven} <br />`;
+			
+			// Count without IP filtering.
+			counter = {};
+			
+			snapshot.forEach(function(childSnapshot) {
+				var key1 = childSnapshot.child('vote1').val();
+				counter[key1] = (counter[key1] || 0) + 1;
+				var key2 = childSnapshot.child('vote2').val();
+				counter[key2] = (counter[key2] || 0) + 1;
+			});
+			cecelia = counter['one'] || 0;
+			matthew = counter['two'] || 0;
+			maria = counter['three'] || 0;
+			sven = counter['four'] || 0;
+			
+			unfiltered_results = `Cecelia: ${cecelia} <br /> Matthew: ${matthew} <br /> Maria: ${maria} <br /> Sven: ${sven} <br />`;
+			return { text:  'Filtered:<br />' + filtered_results + 'Unfiltered:<br />' + unfiltered_results};
+		} else {
+			throw new functions.https.HttpsError('permission-denied', 'You do not have permission to see the results.');
+		}
+		
+	});
 });
--- public/index.js
+++ public/index.js
@@ -12,12 +12,20 @@ const firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+if (window.location.href == 'http://localhost:5000/') {
+	firebase.functions().useFunctionsEmulator('http://localhost:5001');
+}
+
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     document.getElementById('authcontent').hidden = false;
 	document.getElementById('signinreminder').hidden = true;
 	document.getElementById('email').innerHTML = 'Signed in as ' + user.displayName +
 		' (' + user.email + ')';
+	var viewResultsCallable = firebase.functions().httpsCallable('viewResults');
+	viewResultsCallable().then(function(result) {
+		document.getElementById('results').innerHTML = result.data.text;
+	});
   } else {
     document.getElementById('authcontent').hidden = true;
 	document.getElementById('signinreminder').hidden = false;
@@ -45,18 +53,13 @@ firebase.auth().onAuthStateChanged(function(user) {
 function submitVote() {
   var votes = [];
   var submitVoteCallable = firebase.functions().httpsCallable('submitVote');
-  if (document.getElementById('one').checked) {
-    votes.push('one');
-  }
-  if (document.getElementById('two').checked) {
-    votes.push('two');
-  }
-  if (document.getElementById('three').checked) {
-    votes.push('three');
-  }
-  if (document.getElementById('four').checked) {
-    votes.push('four');
-  }
+  var ids = ['one', 'two', 'three', 'four'];
+  ids.forEach(function(id) {
+	if (document.getElementById(id).checked) {
+		votes.push(id);
+	}
+  });
+  
   submitVoteCallable({votes: votes}).then(function(result) {
     document.getElementById('statusmsg').innerHTML = result.data.text;
     document.getElementById('errormsg').innerHTML = '';
